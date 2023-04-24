3ce4cfe42d9bafed79b89ae8888dede640729ad2
Аббасгулиев Ролан Мутаризович <abbasgulyev@local.reshenie.spb.ru>
2023-04-24 17:30:35 +0300

    init proj

--- src/app/components/weather-short/weather-short.component.ts
+++ src/app/components/weather-short/weather-short.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IWeather } from '../../models/weather';
 @Component({
   selector: 'app-weather-short',
   templateUrl: './weather-short.component.html',
   styleUrls: ['./weather-short.component.css'],
 })
-export class WeatherShortComponent {
-  
+export class WeatherShortComponent implements OnInit {
+  @Input() weatherInfo: IWeather | null;
+
+  tempreture: number | undefined;
+
+  getTempreture(): void {
+    console.log(this.weatherInfo);
+    this.tempreture =
+      this.weatherInfo?.timelines[0].intervals[0].values.tempreture;
+  }
+
+  ngOnInit(): void {
+    this.getTempreture();
+  }
 }
